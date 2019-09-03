2c43030648dd2c56a3156d35221044400b7552a9
ppthreefive@gmail.com <pham184535>
2019-09-03 01:45:48 -0700

    Implemented Insertion Sort as a working function.

--- Encode/Encode/Encode.cpp
+++ Encode/Encode/Encode.cpp
@@ -2,7 +2,40 @@
 #include <string>
 using namespace std;
 
+string insertionSort(string line) 
+{
+	char temp;
+	int i, j = 0;
+	int lowerBound = 0;
+	int upperBound = line.length() - 1;
+
+	for (i = lowerBound; i <= upperBound; i++)
+	{
+		temp = line.at(i);
+
+		for (j = i - 1; j >= lowerBound; j--)
+		{
+			if (line.at(j) <= temp) 
+			{
+				break;
+			}
+
+			line.at(j + 1) = line.at(j);
+		}
+
+		line.at(j + 1) = temp;
+	}
+
+	return line;
+}
+
 int main()
 {
+	string input;
 
+	cout << "Enter string to be sorted: ";
+	cin >> input;
+	cout << insertionSort(input);
+
+	return 0;
 }
